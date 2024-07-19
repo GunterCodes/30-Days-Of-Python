4486582db1f953d3b9c77ede09aa4867d379b21e
GunterCodes <114779580+GunterCodes@users.noreply.github.com>
2024-07-18 20:28:13 -0400

    Create gunter_variables.py
    Created variables per Day 2 - Exercise 1

---
+++ 02_Day_Variables_builtin_functions/gunter_variables.py
@@ -0,0 +1,12 @@
+#Day 2: 30 Days of Python Programming
+
+first_name = "Gunter"
+last_name = "Codes"
+full_name = first_name + " " + last_name
+country = "USA"
+city = "Gotham"
+age = "200"
+year = "1970"
+is_married = False
+is_true = True
+is_light_on = False
